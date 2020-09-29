0d253468eac7cafa0f2e295d024da47c59d8a7f5
quanvg <vqa.swe@gmail.com>
2020-09-29 06:23:39 -0400

    test filtered background

--- src/App.js
+++ src/App.js
@@ -5,20 +5,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo"
-          alt="logo:sorry that's the only pic of myself I have at the moment after building my first PC" />
-        <p>
-          My name is <code>Quan</code> and I'm going to master <code>React</code> !!!
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/quanvg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check Me Out!
-        </a>
+      <header className="App-header">       
+        {/* {<img src={logo} className="App-logo"} alt="logo" /> */}
+        <div className="container">
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <p>
+            My name is <code>Quan</code> and I'm going to master <code>React</code> !!!
+          </p>
+          <a
+            className="App-link"
+            href="https://github.com/quanvg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Check Me Out!
+          </a>
+        </div>
       </header>
     </div>
   );
