12a22013f5012559ae75c009da36a8c3ecf75041
quanvg <vqa.swe@gmail.com>
2020-09-29 04:01:38 -0400

    modified App.js to say {insert your name here}: (edit the <p> tag on line 11)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name is <code>Quan</code> and I'm going to master <code>React</code> !!!
         </p>
         <a
           className="App-link"
