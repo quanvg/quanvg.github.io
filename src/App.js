4fa32cbf6caa2bd6b2f029bfd499e71fd7d3e71f
quanvg <vqa.swe@gmail.com>
2020-09-29 04:31:38 -0400

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/quanvg"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check Me Out!
         </a>
       </header>
     </div>
