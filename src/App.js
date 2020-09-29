c4a73ae851b87db55a2061d9521e0574efa54c2c
quanvg <vqa.swe@gmail.com>
2020-09-29 04:28:12 -0400

    added my personal image to App.js. Alt-text was also changed to explain why that's the only image of myself I could find at the moment haha

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from "./Quan.png";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo"
+          alt="logo:sorry that's the only pic of myself I have at the moment after building my first PC" />
         <p>
           My name is <code>Quan</code> and I'm going to master <code>React</code> !!!
         </p>
